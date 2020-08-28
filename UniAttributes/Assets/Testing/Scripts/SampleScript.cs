0099731d6c7719935ca3a7117630081eb0aee32f
Fofanius <vova19007@gmail.com>
2020-08-28 21:46:42 +0300

    Атрибут для вызова метода
    Базовая версия атрибута и инспектора, для вызова методов, помеченного специальным атрибутом

---
+++ UniAttributes/Assets/Testing/Scripts/SampleScript.cs
@@ -0,0 +1,19 @@
+﻿using UniAttributes.Runtime.Scripts;
+using UnityEngine;
+
+public class SampleScript : MonoBehaviour
+{
+    public int IntValue;
+
+    [Button(Name = "Some Method")]
+    public void Method()
+    {
+        Debug.Log("Message");
+    }
+
+    [Button(Name = "Some Method 123")]
+    public void Method1(int number)
+    {
+        Debug.Log("Message " + number);
+    }
+}
